a2a01dc1437a94b9ecc5a8ed6849eea799c735a5
anmol.s <anmol.s@zohocorp.com>
2019-02-19 16:15:28 +0530

    Event Managment

---
+++ app/src/main/java/com/show/singlecanvas/ShapeObject.kt
@@ -0,0 +1,25 @@
+package com.show.singlecanvas
+
+import android.graphics.Paint
+import android.graphics.Path
+
+
+sealed class DrawableView {
+
+    data class ShapeObject(
+            val paint: Paint = Paint(),
+            var left: Float = 0f,
+            var top: Float = 0f,
+            val path: Path = Path()
+    ) : DrawableView()
+
+    data class ShapeBBoxObject(
+            val paint: Paint = Paint(),
+            var left: Float = 0f,
+            var top: Float = 0f,
+            val path: Path = Path()
+    ) : DrawableView() {
+        constructor(shapeObject: ShapeObject, paint: Paint) : this(paint, shapeObject.left, shapeObject.top, shapeObject.path)
+    }
+}
+
